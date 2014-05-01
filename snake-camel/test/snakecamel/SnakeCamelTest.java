02427a9a165924b40f13226991f8d0ed03c21a33
g12961ym <g12961ym+git@s20614.tsuda.ac.jp>
2014-05-01 17:27:21 +0900

    snakecamelテストの作成

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -1,14 +1,24 @@
 package snakecamel;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class SnakeCamelTest {
-
-	@Test
-	public void test() {
-		
-	
-	}
+		@Test
+		public void snakeToCamelcaseÇé¿çs() {
+			SnakeCamelUtil calc = new SnakeCamelUtil();
+			String expected = "HappyBirthDay";
+			String actual = calc.snakeToCamelcase("happy_birth_day_ ");
+			assertThat(actual, is(expected));
+		}
+		@Test
+		public void camelToSnakecaseÇé¿çs() {
+			SnakeCamelUtil calc = new SnakeCamelUtil();
+			String expected = "happy_birth_day";
+			String actual = calc.camelToSnakecase("HappyBirthDay");
+			assertThat(actual, is(expected));
+		}	
 }
+
