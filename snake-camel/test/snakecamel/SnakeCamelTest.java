4f167292ab7f2ba3e4842498946aeca6f69b3b08
g12961ym <g12961ym+git@s20614.tsuda.ac.jp>
2014-05-02 17:55:36 +0900

    テストの修正

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -6,11 +6,35 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SnakeCamelTest {
+    	@Test
+    	public void capitalizeを実行() {
+    		String expected = "Happy";
+    		String actual = SnakeCamelUtil.capitalize("happy");
+    		assertThat(actual, is(expected));
+	}    	@Test
+	public void capitalize空行を実行() {
+		String expected = "";
+		String actual = SnakeCamelUtil.capitalize("");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+		public void uncapitalizeを実行() {
+		String expected = "happy";
+			String actual = SnakeCamelUtil.uncapitalize("Happy");
+			assertThat(actual, is(expected));
+	}		
+  		@Test
+    	public void uncapitalize空行を実行() {
+    		String expected = "";
+    		String actual = SnakeCamelUtil.uncapitalize("");
+    		assertThat(actual, is(expected));
+    }	
 		@Test
 		public void snakeToCamelcaseを実行() {
 			SnakeCamelUtil calc = new SnakeCamelUtil();
 			String expected = "HappyBirthDay";
-			String actual = calc.snakeToCamelcase("happy_birth_day_ ");
+			String actual = calc.snakeToCamelcase("_happy_birth_day_ ");
 			assertThat(actual, is(expected));
 		}
 		@Test
@@ -19,6 +43,6 @@ public class SnakeCamelTest {
 			String expected = "happy_birth_day";
 			String actual = calc.camelToSnakecase("HappyBirthDay");
 			assertThat(actual, is(expected));
-		}	
+		}
 }
 
